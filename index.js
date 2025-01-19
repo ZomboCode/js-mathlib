b082d566d9b6cdd23e7c434d40dabe3332c4e56b
Ralf Robert Paabo <ralfrobertp@gmail.com>
2025-01-19 12:18:54 +0200

    update

--- index.js
+++ index.js
@@ -10,4 +10,15 @@ n.printOut();
 m.printOut();
 c.printOut();
 
+var A = new DiagMatrix([1, 2, 4]);
+var b = new ColMatrix([1, 2, 5]);
+
+var x = A.solveLinearSystem(b);
+var x2 = A.solveLinearGE(b);
+var x3 = A.solveLinearSystem2(b);
+
+x.printOut()
+x2.printOut()
+x3.printOut()
+
 module.exports = {vect3d, vect2d, ColMatrix, Matrix, RowMatrix, DiagMatrix}
--- matrix.js
+++ matrix.js
@@ -5,6 +5,24 @@ class Matrix {
         this.h = data.length;
     }
 
+    deepCopy() {
+        var newData = []
+        if (this instanceof DiagMatrix) {
+            for (var i = 0; i < this.h; i++) {
+                newData.push(this.diagonal[i]);
+            }
+            return new DiagMatrix(newData);
+        }
+        
+        for (var i = 0; i < this.h; i++) {
+            newData.push([]);
+            for (var j = 0; j < this.w; j++) {
+                newData[i].push(this.data[i][j])
+            }
+        }
+        return new Matrix(newData);
+    }
+
     /**
      * Function to multiply two matrices
      * @param {*} matrix2 Second matrix to multiply with
@@ -107,7 +125,7 @@ class Matrix {
      * Function to get the transpose of the matrix
      * @returns Transpose of the matrix as a new matrix
      */
-    getTranspose() {
+    transpose() {
         var ans = [];
         if (this instanceof DiagMatrix) {
             for (var i = 0; i < this.h; i++) {
@@ -128,7 +146,7 @@ class Matrix {
      * Function to get inverse of a matrix, currently works for only diagonal matrices
      * @returns Inverse of the matrix as a new matrix
      */
-    getInverse() {
+    inverse() {
         var ans = [];
         if (this instanceof DiagMatrix) {
             for (var i = 0; i < this.h; i++) {
@@ -160,49 +178,62 @@ class Matrix {
         console.log();
     }
 
-    /**
-     * Solves the matrix equation Ax = b using Jacobi method
-     * @param {Matrix} rightSide The vector b on right side of the equation
-     * @returns vector x 
-     */
-    solveLinearSystem(rightSide) {
+    solveDiagonalSystem(rightSide) {
+        var ans = [];
+        for (var i = 0; i < this.w; i++) {
+            ans.push(rightSide.data[i][0]/this.diagonal[i]);
+        }
+        return new DiagMatrix(ans);
+    }
+
+    checkLinearSystem(rightSide) {
         if (!(this.w == this.h && this.w == rightSide.h)) {
             return false;
         }
-        var ans = [];
-        var ans2 = [];
+        if (this instanceof DiagMatrix) {return this.solveDiagonalSystem(rightSide)}
+        return true;
+    }
+
+    /**
+     * Solves the matrix equation Ax = b using Jacobi method
+     * @param {Matrix} rightSide The vector b on right side of the equation
+     * @param {Number} [maxIters=50] Maximum number of iterations
+     * @param {Number} [errorMargin=1e-8] Biggest difference during one iteration to quit iterations
+     * @returns vector x 
+     */
+    solveLinearSystem(rightSide, maxIters = 50, errorMargin = 1e-9) {
+        var check = this.checkLinearSystem(rightSide);
+        if (check != true) {return check;}
+
+        var ans = []; //current estimate for x
+        var ans2 = []; //next estimate for x
         for (var i = 0; i < this.w; i++) {
             ans.push(0);
-            ans2.push(0);
         }
 
-        var maxIters = 50;
-        if (rightSide.length == 0) {
-            maxIters = 1;
-        }
         for (var i = 0; i < maxIters; i++) {
+            var errorSum = 0;
             for (var j = 0; j < this.w; j++) {
-                var value = rightSide.data[j][0];
+                var value = rightSide.data[j][0]; //initial value from b
                 for (var k = 0; k < this.w; k++) {
                     if (k != j) {
-                        value -= ans[k]*this.data[j][k];
+                        value -= ans[k]*this.data[j][k]; //sum of components
                     }
                 }
-                if (this.data[j][j] == 0) {
+                if (this.data[j][j] != 0) { //divide by value on diagonal
                     ans2.push(value/this.data[j][j]);
                 }
-                else {
-                    ans2.push(0);
-                }
-                
+                else {ans2.push(0);}
+                errorSum += (ans2[j]-ans[j])**2/(ans2[j]**2)/this.w;
             }
-            ans = ans2;
+            ans = ans2; //set new value for x
             ans2 = [];
+            //condition for quitting loop
+            if (rightSide.h <= 1 || errorSum < errorMargin**2) {
+                break;
+            }
         }
-        var ansMatrix = new Matrix([ans]).getTranspose();
-        console.log("error");
-        console.log(this.multiply(ansMatrix).add(rightSide.multiplyScalar(-1)));
-        console.log(rightSide);
+        var ansMatrix = new ColMatrix(ans);
         return ansMatrix;
 
     }
@@ -210,101 +241,48 @@ class Matrix {
     /**
      * Solves the matrix equation Ax = b using Gauss-Seidel method
      * @param {Matrix} rightSide The result vector b of length m
+     * @param {Number} [maxIters=50] Maximum number of iterations
+     * @param {Number} [errorMargin=1e-12] Maximal error to quit iterations
      * @returns vector x 
      */
-    solveLinearSystem2(rightSide) {
-        if (!(this.w == this.h && this.w == rightSide.h)) {
-            return false;
-        }
+    solveLinearSystem2(rightSide, maxIters = 50, errorMargin = 1e-9) {
+        var check = this.checkLinearSystem(rightSide);
+        if (check != true) {return check;}
+
         var ans = [];
         var ans2 = [];
         for (var i = 0; i < this.w; i++) {
             ans.push(0);
         }
 
-        var maxIters = 500;
-        if (rightSide.data.length == 0) {
-            maxIters = 1;
-        }
-
-        var errors = [];
         var relax = 1.6
         for (var i = 0; i < maxIters; i++) {
-            var isGood = true;
+            var errorSum = 0;
             for (var j = 0; j < this.w; j++) {
                 var value = rightSide.data[j][0];
                 for (var k = 0; k < this.w; k++) {
-                    if (k < j) {
+                    if (k < j) { //take new value
                         value -= ans2[k]*this.data[j][k];
                     }
-                    else if (k > j) {
+                    else if (k > j) { //take old value
                         value -= ans[k]*this.data[j][k];
                     }
                 }
                 var new_val = ans[j]*(1-relax) + relax*value/this.data[j][j];
-                if (this.data[j][j] == 0) {
-                    new_val = 0;
-                }
+                if (this.data[j][j] == 0) {new_val = 0;}
                 ans2.push(new_val);
-                if (Math.abs(new_val - ans[j]) > Math.abs(rightSide[j])*0.0001) {
-                    isGood = false;
-                }
+                errorSum += (ans2[j]-ans[j])**2/(ans2[j]**2)/this.w;
+                console.log(new_val, i, value, this.data[j][j]);
             }
-            ans = ans2;
+            ans = ans2; //set new value for x
             ans2 = [];
-            if (isGood) { //condition to stop iterating
-                //break;
+            //condition for quitting loop
+            if (rightSide.h <= 1 || errorSum < errorMargin**2) {
+                break;
             }
         }
-        var ansMatrix = new Matrix([ans]).getTranspose();
-        //console.log("error");
-        //console.log(this.multiply(ansMatrix).add(rightSide.multiplyScalar(-1)));
-        //console.log(rightSide, maxIters);
-        //console.log(errors)
+        var ansMatrix = new ColMatrix(ans);
         return ansMatrix;
-    }
-
-    /**
-     * Solve system of linear equations Ax = b using Conjugate gradient method
-     * @param {Matrix} rightSide the right side vector b 
-     * @returns the searched x
-     */
-    solveLinearGradientConjugate(rightSide) {
-        if (!(this.w == this.h && this.w == rightSide.h)) {
-            return false;
-        }
-        var ans = [];
-        var ans2 = [];
-        for (var i = 0; i < this.w; i++) {
-            ans.push(0);
-        }
-
-        var maxIters = 50;
-        if (rightSide.data.length == 0) {
-            maxIters = 1;
-        }
-
-        console.log(this.data);
-
-        var ansMatrix = new Matrix([ans]).getTranspose();
-        var residual = rightSide.add(this.multiply(ansMatrix).multiplyScalar(-1));
-        var direction = new Matrix(residual.data);
-        for (var i = 0; i < maxIters; i++) {
-            var alpha = residual.getTranspose().multiply(residual).data[0][0]/(direction.getTranspose().multiply(this).multiply(direction).data[0][0]);
-            ansMatrix = ansMatrix.add(direction.multiplyScalar(alpha));
-            var residual_old = new Matrix(residual.data);
-            var residual = residual_old.add(this.multiply(direction).multiplyScalar(-alpha));
-            var beta = residual.multiply(residual.getTranspose()).data[0][0]/(residual_old.getTranspose().multiply(residual_old).data[0][0]);
-            var direction = residual.add(direction.multiplyScalar(beta));
-
-
-        }
-        console.log("error");
-        //console.log(this.multiply(ansMatrix).add(rightSide.multiplyScalar(-1)));
-        //console.log(rightSide);
-        return ansMatrix;
-
-
     }
 
     /**
@@ -312,30 +290,33 @@ class Matrix {
      * @param {Matrix} rightSide the right side vector b
      * @returns vector x
      */
-    solveGaussianElimination(rightSide) {
-        if (!(this.w == this.h && this.w == rightSide.h)) {
-            return false;
-        }
-        var newMatrix = this.data;
-        var newRightSide = rightSide.data;
+    solveLinearGE(rightSide) {
+        var check = this.checkLinearSystem(rightSide);
+        if (check != true) {return check;}
+
+        var newMatrix = this.deepCopy().data; //make array from Matrix object for editing
+        var newRightSide = rightSide.deepCopy().data;
         //this.printOut()
         //rightSide.printOut()
         for (var i = 0; i < this.data.length; i++) {
-            var pivotId = i;
+            var pivotId = i; //find the row that has pivot (highest absolute value)
             for (var j = i; j < this.data.length; j++) {
                 if (Math.abs(newMatrix[i][j]) > Math.abs(newMatrix[i][pivotId])) {
                     pivotId = j;
                 }
             }
-
+            //move the pivot row to i-th row
             var line = newMatrix[pivotId];
             newMatrix.splice(pivotId, 1);
             newMatrix.splice(i, 0, line);
+            //must do same with the right side vector
             var val = newRightSide[pivotId]
             newRightSide.splice(pivotId, 1);
             newRightSide.splice(i, 0, val);
+
+            //do replacement to add next row to triangular matrix
             for (var k = i; k < this.data.length; k++) {
-                var pivotPower = newMatrix[k][i]/newMatrix[i][i];
+                var pivotPower = newMatrix[k][i]/newMatrix[i][i]; //how much to add per each row
                 if (k == i) {
                     continue;
                 }
@@ -346,11 +327,8 @@ class Matrix {
             }
             
         }
-        var matr = new Matrix(newMatrix);
-        //matr.printOut();
-        var vec = new Matrix([newRightSide]);
-        //vec.printOut();
         //now that i have triangular matrix, solve one by one
+        //start from last and end up with first equation
         var ans = [];
         for (var i = this.data.length-1; i >= 0; i--) {
             var coord = newRightSide[i]/newMatrix[i][i];
@@ -360,10 +338,47 @@ class Matrix {
                 newMatrix[j][i] = 0;
             }
         }
+        //reverse for correct order
         ans.reverse();
-        var ansMatrix = new Matrix([ans]).getTranspose();
-        //ansMatrix.printOut()
+        var ansMatrix = new ColMatrix(ans);
         return ansMatrix;
+    }
+
+    /**
+     * Solve system of linear equations Ax = b using Conjugate gradient method
+     * @param {Matrix} rightSide the right side vector b 
+     * @returns the searched x
+     */
+    solveLinearGradient(rightSide, maxIters = 50) {
+        var check = this.checkLinearSystem(rightSide);
+        if (check != true) {return check;}
+
+        var ans = [];
+        var ans2 = [];
+        for (var i = 0; i < this.w; i++) {
+            ans.push(0);
+        }
+
+        console.log(this.data);
+
+        //highly schizophrenic
+        var ansMatrix = new Matrix([ans]).transpose();
+        var residual = rightSide.add(this.multiply(ansMatrix).multiplyScalar(-1));
+        var direction = new Matrix(residual.data);
+        for (var i = 0; i < maxIters; i++) {
+            var alpha = residual.transpose().multiply(residual).data[0][0]/(direction.transpose().multiply(this).multiply(direction).data[0][0]);
+            ansMatrix = ansMatrix.add(direction.multiplyScalar(alpha));
+            var residual_old = new Matrix(residual.data);
+            var residual = residual_old.add(this.multiply(direction).multiplyScalar(-alpha));
+            var beta = residual.multiply(residual.transpose()).data[0][0]/(residual_old.transpose().multiply(residual_old).data[0][0]);
+            var direction = residual.add(direction.multiplyScalar(beta));
+
+
+        }
+        console.log("error");
+        return ansMatrix;
+
+
     }
 }
 
